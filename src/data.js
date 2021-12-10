2caffd827a9255aed30966779764cf7a3f92f571
maxkram <max.kramer@live.com>
2021-12-09 20:36:02 -0500

    first one

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'Luna',
+    job: 'French Bulldog',
+    image:
+      'https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?cs=srgb&dl=pexels-charles-1851164.jpg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280',
+    text: 'When you wake up, I will look at you that way until you give me something to eat',
+  },
+  {
+    id: 2,
+    name: 'Milo',
+    job: 'German Shepherd Dog',
+    image:
+      'https://images.pexels.com/photos/1805164/pexels-photo-1805164.jpeg?cs=srgb&dl=pexels-valeria-boltneva-1805164.jpg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280',
+    text: 'Stroke me, play with me, watch me. I am the best',
+  },
+  {
+    id: 3,
+    name: 'Jack',
+    job: 'Labrador Retriever',
+    image:
+      'https://images.pexels.com/photos/2023384/pexels-photo-2023384.jpeg?cs=srgb&dl=pexels-dominika-roseclay-2023384.jpg&fm=jpg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280',
+    text: "Who is everyone's favorite? I AM.",
+  },
+  {
+    id: 4,
+    name: 'Zoe',
+    job: 'Golden Retriever',
+    image:
+      'https://images.pexels.com/photos/1458916/pexels-photo-1458916.jpeg?cs=srgb&dl=pexels-poodles-doodles-1458916.jpg&fm=jpg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280',
+    text: 'I am still small. But my mother said that I will grow up a bigger and more terrible dog. Rrrr ...',
+  },
+];
+
+export default reviews;
